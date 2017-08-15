771a427bf308d2decc513dbeec6700002204af45
aniket2398 <30971529+aniket2398@users.noreply.github.com>
2017-08-15 10:02:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById("main-text");
 
 element.innerHTML = 'New value';
 
-var img = docment.getElementById('img');
+var img = docment.getElementById('modi');
 img.onclick = function(){
     img.style.marginLeft =  '100px';
 }
